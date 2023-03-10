22d2df64c36b15a661b23a9129f4f12ffbf779c3
ChulJin Lim <cjfwls9806@gmail.com>
2023-03-10 18:22:04 +0900

    [setting] 세팅 추가 - 폰트 cdn 추가 - react-slick 추가 - BaseLayout style 설정 - rem 유틸 함수 생성

--- src/theme/utils.js
+++ src/theme/utils.js
@@ -3,12 +3,22 @@ import { tokens } from './tokens';
 /**
  * Color 변환 유틸 함수
  * put string in parameters
- * @param {string} (ex: 'gray-100')
- * @returns {{tokens[colorName]}} (ex: #e1e1e1)
+ * @param {string} color (ex: 'gray-100')
+ * @returns {string} color (ex: #e1e1e1)
  */
 export const getColor = (colorNameAndVariant) => {
   if (colorNameAndVariant.includes('-')) {
     let colorName = colorNameAndVariant;
     return tokens[colorName];
   }
+};
+
+/**
+ * rem 단위 변환 유틸 함수
+ * @param {string} value (ex: rem(16))
+ * @param {string} baseValue
+ * @returns {{value / baseValue}} (ex: 1rem)
+ */
+export const rem = (value, baseValue = 16) => {
+  return `${Number(value / baseValue)}rem`;
 };
