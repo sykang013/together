4ebef93dc87bfbb580b8eb66666a972ccbf470c2
seob <kseob7@naver.com>
2023-03-14 00:09:01 +0900

    [faet] ProfileEdit 적용
    - 컴포넌트 재활용해서 적용하기
    
    #16

--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -1,7 +1,39 @@
+import {
+  StProfileTitle,
+  StProfileSubTitle,
+  StProfileItems,
+  StProfileItem,
+  StProfileImage,
+  StProfileButton,
+} from '@/components/profile/Profile';
 import React from 'react';
 
-const ProfileEdit = () => {
-  return <div>test</div>;
+const ProfilePage = () => {
+  return (
+    <>
+      <StProfileTitle>프로필 편집</StProfileTitle>
+      <StProfileSubTitle>편집할 프로필을 선택해주세요.</StProfileSubTitle>
+      <StProfileItems>
+        <StProfileItem>
+          <StProfileImage />
+          <p>test</p>
+        </StProfileItem>
+        <StProfileItem>
+          <StProfileImage />
+          <p>test</p>
+        </StProfileItem>
+        <StProfileItem>
+          <StProfileImage />
+          <p>test</p>
+        </StProfileItem>
+        <StProfileItem>
+          <StProfileImage />
+          <p>test</p>
+        </StProfileItem>
+      </StProfileItems>
+      <StProfileButton select>완료</StProfileButton>
+    </>
+  );
 };
 
-export default ProfileEdit;
+export default ProfilePage;
