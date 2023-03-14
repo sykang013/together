d0052006c23b5f9ea824053a91124eecf41fb124
guahyoung <kooay93@gmail.com>
2023-03-14 22:29:48 +0900

    [code review] 코드리뷰 수정사항
    - mainpage에서 LogoutModal import 안하기

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,9 @@
 import Header from '@/components/header/Header';
-import LogoutModal from '@/components/logout/LogoutModal';
 
 const MainPage = () => {
   return (
     <>
       <Header />
-      <LogoutModal />
     </>
   );
 };
