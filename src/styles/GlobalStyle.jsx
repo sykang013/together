30c78e0a1aa4c9edca5e9a962ff0abdc1057413a
ChulJin Lim <cjfwls9806@gmail.com>
2023-03-15 14:08:01 +0900

    [feat] GlobalStyle.jsx 변경 - var()를 쓰기 위한 root 에 컬러 추가
    #18

--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -2,8 +2,8 @@ import { getColor } from '@/theme/utils';
 import { createGlobalStyle } from 'styled-components/macro';
 
 const GlobalStyle = createGlobalStyle`
-  body {
-  background-color : ${getColor('--black')};
+body {
+  background-color : ${getColor('--white')};
   button {cursor: pointer};
 }
   body,
@@ -26,6 +26,24 @@ const GlobalStyle = createGlobalStyle`
     width: 100%;
     height: auto;
 }
+
+  :root{
+    --black: #000000;
+    --white: #ffffff;
+    --gray100: #e1e1e1;
+    --gray200: #c4c4c4;
+    --gray300: #a6a6a6;
+    --gray400: #898989;
+    --gray500: #6b6b6b;
+    --gray600: #565656;
+    --gray700: #404040;
+    --gray800: #2b2b2b;
+    --gray900: #151515;
+    --primary: #182FFF;
+    --secondary: #1728C0;
+    --dark-bg1: #191919;
+    --dark-bg2: #212121;
+  }
 `;
 
 export default GlobalStyle;
