c5b361f4dd3604dbe3fb696357c41b774a354827
senasoon <senasoon0@gmail.com>
2023-03-16 13:34:20 +0900

    [setting] storybook styled-components 적용 - addon-styling 설치 - main.js, preview.js 설정 추가

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,61 @@
 /** @type { import('@storybook/react').Preview } */
 import { withRouter } from 'storybook-addon-react-router-v6';
+import { withThemeFromJSXProvider } from '@storybook/addon-styling';
+import { createGlobalStyle } from 'styled-components';
+import { getColor } from '../src/theme/utils';
+
+const GlobalStyles = createGlobalStyle`
+body {
+  background-color : ${getColor('--black')};
+  button,
+  a {cursor: pointer};
+  color: ${getColor('--white')};
+}
+  body,
+  body::before,
+  body::after,
+  body *,
+  body *::before,
+  body *::after {
+    box-sizing: border-box;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+  video {
+    width: 100%;
+    height: auto;
+}
+
+  :root{
+    --black: #000000;
+    --white: #ffffff;
+    --gray100: #e1e1e1;
+    --gray200: #c4c4c4;
+    --gray300: #a6a6a6;
+    --gray400: #898989;
+    --gray500: #6b6b6b;
+    --gray600: #565656;
+    --gray700: #404040;
+    --gray800: #2b2b2b;
+    --gray900: #151515;
+    --primary: #182FFF;
+    --secondary: #1728C0;
+    --dark-bg1: #191919;
+    --dark-bg2: #212121;
+  }
+`;
 
 export const decorators = [
-  withRouter
+  withRouter,
+  withThemeFromJSXProvider({
+    GlobalStyles, // Adds your GlobalStyle component to all stories
+  }),
 ];
 
 export const parameters = {
@@ -48,8 +101,8 @@ const preview = {
       },
     },
     viewport: {
-      viewports: customViewports
-    }
+      viewports: customViewports,
+    },
   },
 };
 
