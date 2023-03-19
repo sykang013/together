bfe0f966ad566735cdfc8a077cfbaaf9058b2473
ChulJin Lim <cjfwls9806@gmail.com>
2023-03-19 21:28:46 +0900

    [style] Footer 마크업 및 디자인 - Footer.jsx 마크업 및 디자인 - 메인페이지에 footer 추가
    #45

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,11 @@
+import Footer from '@/components/footer/Footer';
 import Header from '@/components/header/Header';
-import { IconButton } from '@/components/landing/IconButton';
 
 const MainPage = () => {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 };
