27e19e79bffdf89ce7e2e69ea42167130af5f951
guahyoung <kooay93@gmail.com>
2023-03-21 14:09:14 +0900

    [code review] 코드리뷰 수정사항
    - useState값 false로 변경
    -openModal,closeModa 함수 생성
    - useEffect mainPage로 이동
    
    #42

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,23 +1,37 @@
+// import Footer from '@/components/footer/Footer';
 import Header from '@/components/header/Header';
 import Popup from '@/components/popup/Popup';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 const MainPage = () => {
-  const [showModal, setShowModal] = useState(true);
+  const [showModal, setShowModal] = useState(false);
 
-  const handleCloseModal = () => {
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
     setShowModal(false);
   };
+
+  useEffect(() => {
+    const expiryDate = JSON.parse(localStorage.getItem('visitCookieExpiry'));
+    if (expiryDate) {
+      const currentDate = new Date().getTime();
+
+      if (currentDate > expiryDate) {
+        localStorage.removeItem('visitCookieExpiry');
+        openModal();
+      }
+    } else {
+      openModal();
+    }
+  }, []);
 
   return (
     <>
       <Header />
-      {showModal && (
-        <Popup
-          OnClose={() => {
-            handleCloseModal();
-          }}
-        />
-      )}
+      {/* <Footer /> */}
+      {showModal && <Popup closeModal={closeModal} />}
     </>
   );
 };
