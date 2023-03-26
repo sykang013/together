df49776a6096b23d7e754068c2dc10419ab57bb8
ChulJin Lim <cjfwls9806@gmail.com>
2023-03-26 21:52:04 +0900

    [feat] 404 페이지 - 404 not found 구현
    #91

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,21 +1,46 @@
 import { getFontStyle, rem } from '@/theme/utils';
 import { Helmet } from 'react-helmet-async';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components/macro';
 
-const StNotFound = styled.p`
-  ${getFontStyle('headingXXXL')};
-  text-align: center;
-  margin-top: ${rem(300)};
+const StNotFoundContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
-
-const StLink = styled(Link)`
-  text-decoration: underline;
-  :hover {
-    color: var(--primary);
+const StNotFound = styled.div`
+  ${getFontStyle('headingXXXL')};
+  line-height: 120%;
+  text-align: center;
+  width: ${rem(800)};
+  margin-top: ${rem(180)};
+  padding: ${rem(90)} 0;
+  box-shadow: -5px -5px 100px 5px #2c2b2b;
+  border-radius: 10px;
+  div {
+    margin-top: ${rem(20)};
+  }
+  span {
+    ${getFontStyle('ParagraphXL')};
+    font-weight: 200;
   }
 `;
+
+const StButton = styled.button`
+  padding: ${rem(10)};
+  background-color: var(--primary);
+  color: var(--white);
+  border: none;
+  border-radius: 4px;
+  width: ${rem(288)};
+  ${getFontStyle('headingXL')};
+  &:hover {
+    background-color: var(--secondary);
+  }
+`;
+
 const NotFound = () => {
+  const navigate = useNavigate();
   return (
     <>
       <Helmet>
@@ -25,16 +50,21 @@ const NotFound = () => {
           content="페이지가 존재하지 않거나, 사용할 수 없는 페이지입니다."
         />
       </Helmet>
-      <StNotFound>
-        ⚠️404 not found⚠️ 없는 페이지 입니다.
-        <br />
-        페이지가 존재하지 않거나, 사용할 수 없는 페이지입니다.
-        <br />
-        입력하신 주소가 정확한지 다시 한번 확인해 주시기 바랍니다.
-        <br />
-        {`클릭 -> `}
-        <StLink to="/">메인 페이지</StLink>로 이동
-      </StNotFound>
+      <StNotFoundContainer>
+        <StNotFound>
+          ⚠️404 not found!⚠️
+          <br />
+          <div>
+            <span>페이지가 존재하지 않거나, 사용할 수 없는 페이지입니다.</span>
+            <br />
+            <span>
+              입력하신 주소가 정확한지 다시 한번 확인해 주시기 바랍니다.
+            </span>
+          </div>
+          <br />
+          <StButton onClick={() => navigate('/')}>홈으로 이동</StButton>
+        </StNotFound>
+      </StNotFoundContainer>
     </>
   );
 };
