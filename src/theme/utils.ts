f854d9d02f6f4a835ce2e5b94257bd83ebd21f72
sykang013 <sy.kang013@gmail.com>
2023-04-12 10:20:24 +0900

    [bug] getFontStyle 에러 해결 - overload 에러 해결 #125

--- src/theme/utils.ts
+++ src/theme/utils.ts
@@ -1,22 +1,10 @@
 import { tokens } from './tokens';
 
-/**
- * rem 단위 변환 유틸 함수
- * @param {string} value (ex: rem(16))
- * @param {string} baseValue
- * @returns {{value / baseValue}} (ex: 1rem)
- */
 export const rem = (value: number | string, baseValue = 16): string => {
   return `${Number(value) / baseValue}rem`;
 };
 
-/**
- * FontStyle 변환 유틸 함수
- * put string in parameters
- * @param {string} fontStyleAndVariant (ex: 'headingM')
- * @returns {string} fontStyle
- */
-export const getFontStyle = (fontStyleAndVariant: string): object => {
+export const getFontStyle = (fontStyleAndVariant: string) => {
   const fontStyles = fontStyleAndVariant;
   return tokens.fonts[fontStyles];
 };
