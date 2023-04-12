df4df8a164d5de891a02fd0f682b4ed8062612c4
sykang013 <sy.kang013@gmail.com>
2023-04-12 14:35:17 +0900

    [chore] getFontStyle 명시적 타입 IFontTokens 설정 getfontstyle 오류 해결 #125

--- src/theme/utils.ts
+++ src/theme/utils.ts
@@ -1,10 +1,10 @@
-import { tokens } from './tokens';
+import { IFontTokens, tokens } from './tokens';
 
 export const rem = (value: number | string, baseValue = 16): string => {
   return `${Number(value) / baseValue}rem`;
 };
 
-export const getFontStyle = (fontStyleAndVariant: string) => {
+export const getFontStyle = (fontStyleAndVariant: string): IFontTokens => {
   const fontStyles = fontStyleAndVariant;
   return tokens.fonts[fontStyles];
 };
